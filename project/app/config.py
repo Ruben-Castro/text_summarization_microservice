dd1eb27031cec788ae8dc5bec75f95270b92de60
Ruben Castro-Espinoza <rubencastroe@gmail.com>
2023-05-07 01:26:24 -0700

    added postgresql container to project

--- project/app/config.py
+++ project/app/config.py
@@ -1,13 +1,15 @@
 import logging 
+import os 
 from functools import lru_cache
 
-from pydantic import BaseSettings 
+from pydantic import BaseSettings, AnyUrl 
 
 log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     environment :str = "dev"
     testing: bool  = bool(0)
+    database_url:AnyUrl = os.environ.get("DATABASE_URL")
 
 @lru_cache()
 def get_settings() -> BaseSettings:
--- project/app/main.py
+++ project/app/main.py
@@ -10,5 +10,6 @@ async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong",
         "environment": settings.environment,
-        "testing": settings.testing
+        "testing": settings.testing,
+        "database_url": settings.database_url
     }
