0df06a2be49b6953a4a32fa04c6661aaf2cda937
Ruben Castro-Espinoza <rubencastroe@gmail.com>
2023-05-07 12:28:03 -0700

    get summaries/{id} working and tested

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,5 +1,10 @@
+import logging
+
+from typing import Optional
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+
+log = logging.getLogger("uvicorn")
 
 async def post(payload:SummaryPayloadSchema) -> int:
     summary = TextSummary(
@@ -9,3 +14,10 @@ async def post(payload:SummaryPayloadSchema) -> int:
 
     await summary.save()
     return summary.id
+
+async def get(id:int) -> Optional[dict]:
+    summary = await TextSummary.filter(id= id).first().values()
+    log.info(f"Summary= {summary}")
+    if summary:
+        return summary
+    return None 
